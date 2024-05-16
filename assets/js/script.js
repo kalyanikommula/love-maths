ff95f27f8d2d2598eaa01710dc9ba7cb01f714e0
kalyani kommula <kalyanib.tech505@gmail.com>
2024-05-16 12:52:14 +0000

    add some code to checkAnswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for( let button of buttons) {
         button.addEventListener("click", function() {
             if(this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -32,8 +32,20 @@ function runGame(gameType) {
      }
     
 }
+/**
+ * Checks the answer against the first element in
+ * returned CalculateCorrectAnswer array
+ */
 function checkAnswer() {
-
+  let userAnswer = parseInt(document.getElementById("answer-box").value);
+  let calculatedAnswer = calculateCorrectAnswer();
+  let isCorrect = userAnswer === calculatedAnswer[0];
+  if (isCorrect) {
+    alert (" Hey! you got it right! :D");
+  } else {
+    alert (`Awwww.......You shared ${userAnswer}. The correct answer is ${calculatedAnswer[0]}!`);
+  }
+  runGame(calculatedAnswer[1]);
 }
 
 /**
